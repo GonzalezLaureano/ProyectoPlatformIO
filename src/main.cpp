6927d2302f729f6629c0e147c393c76c9ee15883
LAUREANO\laure <laureanogonzalez2020@gmail.com>
2023-03-20 20:55:51 -0300

    inicial commit

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+int contadoraso=0;
+
+void setup() {
+  // put your setup code here, to run once:
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  while (contadoraso<1)
+  {/* code */
+
+  }
+  
+}
