da96abf025e4fb9e3fb178fec30960621176086a
LAUREANO\laure <laureanogonzalez2020@gmail.com>
2023-03-20 21:05:31 -0300

    Termine de hacer que el led se prenda y apague luego de 5 seg

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,15 @@
 #include <Arduino.h>
-int contadoraso=0;
+
+const int PIN_LED=2;
 
 void setup() {
-  // put your setup code here, to run once:
-  
+  pinMode(PIN_LED,OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  while (contadoraso<1)
-  {/* code */
-
-  }
-  
+  digitalWrite(PIN_LED,HIGH);
+  delay(5000);
+  digitalWrite(PIN_LED,LOW);
+  delay(5000);
+  // put your main code here, to run repeatedly: 
 }
