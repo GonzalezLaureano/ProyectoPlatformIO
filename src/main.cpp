42638158d742bc5e807e9cd860aa1bea3583c9e0
LAUREANO\laure <laureanogonzalez2020@gmail.com>
2023-04-03 20:13:36 -0300

    Terminado pero con Delay

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,28 @@
 #include <Arduino.h>
 const int PIN_LED=D0;
+const int PIN_BUTTON_FAST=D1;
+const int PIN_BUTTON_LOW=D2;
+int INT_time=1000;
+
 void setup() {
   pinMode(PIN_LED,OUTPUT);
+  pinMode(PIN_BUTTON_FAST,INPUT);
+  pinMode(PIN_BUTTON_LOW,INPUT);
 }
 
 void loop() {
+  if (digitalRead(PIN_BUTTON_FAST)==HIGH)
+  {
+    INT_time=INT_time-1000;
+  }
+  if (digitalRead(PIN_BUTTON_LOW)==HIGH)
+  {
+    INT_time=INT_time+1000;
+  }
+  
   digitalWrite(PIN_LED,HIGH);
-  delay(5000);
+  delay(INT_time);
   digitalWrite(PIN_LED,LOW);
-  delay(5000);
+  delay(INT_time);
   // put your main code here, to run repeatedly: 
 }
